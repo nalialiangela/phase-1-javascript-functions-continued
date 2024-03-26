ab81aef859966c58d60331a2f206f845f1857f0a
naliali <nalialiangela365@gmail.com>
2024-03-26 12:35:04 +0300

    functions

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+
+function saturdayFun(activity = 'roller-skate'){
+        return  'This Saturday, I want to ${activity}!'
+    }
+    
+
+
+function mondayWork(plan = 'go to the office'){
+    let result = 'This Monday, I will ${plan}.'
+    return result
+}
+
+
+function wrapAdjective(style = '*'){
+    return function (adjective = 'You are *a hard worker*!'){
+        return 'You are ${style}${adjectives}${style}'
+    }
+}
+
+console.log(mondayWork)
+
+
+const awesome = wrapAdjective('||');
+console.log(awesome('awesome'));
